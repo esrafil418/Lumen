9f07cf83534ce982e9dfd8ab19d5930cdcddc1de
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-20 20:01:31 +0330

    feat: Add Responsive Layout with Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { Person2 } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import BlogGrid from "./components/BlogGrid";
+import BlogHeader from "./components/BlogHeader";
 
 export default function App() {
   return (
-    <div>
-      <Button variant="outlined" startIcon={<Person2 />}>Hello MUI</Button>
-    </div>
-  )
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <BlogHeader />
+
+      <Container component="main" sx={{ flex: 1, py: 4 }}>
+        <BlogGrid />
+        {/* <BlogForm /> */}
+      </Container>
+
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          bgcolor: "primary.main",
+          color: "white",
+          mt: "auto",
+        }}
+      >
+        <Container maxWidth="lg">
+          <Typography variant="body2" align="center">
+            © 2026 My Blog. All rights reserved.
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
+  );
 }
